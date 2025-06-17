3f4c29e03e057e938d48b5793d362cbc95d00e61
Oscar Uriel <oscar_c_s@hotmail.com>
2025-06-16 21:42:31 -0600

    My First Counter app in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
+import { Fragment } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  //Todos los return devuelven un solo elemento root
+  //const state = useState(0)
+  //console.log(state)
   const [count, setCount] = useState(0)
-
+  /*
+  Se recompone cada vez que pasa 1s
+  setTimeout(() => {
+    setCount(count+1);
+  }, 1000);
+  */
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+  const handleSubstract = () => {
+    setCount(count - 1)
+  }
+  const handleReset = () => {
+    setCount(0)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Fragment>
+      <div className="container">
+        <h1>Counter {count}</h1>
+        <hr />
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubstract}>-</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Fragment>
+  );
 }
 
 export default App
